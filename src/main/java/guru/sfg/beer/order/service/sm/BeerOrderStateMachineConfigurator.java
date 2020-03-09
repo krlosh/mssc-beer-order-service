365f96a82e37d88d4b95256069ae6a067f60897e
carloshenche <carloshenche@metricool.com>
2020-03-09 15:20:17 +0100

    Added pending states

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfigurator.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfigurator.java
@@ -29,7 +29,7 @@ public class BeerOrderStateMachineConfigurator extends StateMachineConfigurerAda
     public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
         transitions.withExternal()
                     .source(BeerOrderStatusEnum.NEW)
-                    .target(BeerOrderStatusEnum.NEW)
+                    .target(BeerOrderStatusEnum.VALIDATION_PENDING)
                     .event(BeerOrderEventEnum.VALIDATE_ORDER)
             .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW)
