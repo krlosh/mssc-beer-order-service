f2b93cb485c1a6804841bdbf700230c52ce64720
carloshenche <carloshenche@metricool.com>
2020-03-24 13:15:03 +0100

    Included state action configuration

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfiguration.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfiguration.java
@@ -11,6 +11,7 @@ public class JmsConfiguration {
 
     public static final String MY_QUEUE ="my-hello-world";
     public static final String MY_SEND_RCV_QUEUE ="replybacktome";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
 
     @Bean
     public MessageConverter messageConverter(){
